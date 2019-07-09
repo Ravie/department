90e6f6e6681bcb34d6b1a7696a2a153887047bfe
Ravie <ravijkee@yandex.ru>
2019-07-09 16:30:18 +0300

    Check on existing employee when adding new

--- src/main/java/com/github/department/controller/MainController.java
+++ src/main/java/com/github/department/controller/MainController.java
@@ -1,13 +1,17 @@
 package com.github.department.controller;
 
 import com.github.department.entity.Employee;
+import com.github.department.entity.User;
 import com.github.department.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -27,9 +31,12 @@ public class MainController {
     }
 
     @PostMapping("/index")
-    public String addEmployee(@RequestParam String name, @RequestParam Integer depId, @RequestParam Double salary, Model model) {
-        Employee employee = new Employee(name, depId, salary);
-        employeeRepo.save(employee);
+    public String addEmployee(@AuthenticationPrincipal User user, @RequestParam String name, @RequestParam Integer depId, @RequestParam Double salary, Model model) {
+        List<Employee> employeeName = employeeRepo.findByName(name);
+        if (employeeName.isEmpty()) {
+            Employee employee = new Employee(name, depId, salary, user);
+            employeeRepo.save(employee);
+        }
         Iterable<Employee> employeeRepoAll = employeeRepo.findAll();
         model.addAttribute("employees", employeeRepoAll);
         return "index";
