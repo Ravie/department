8e2f48d408082e161af68c141dcf527fa2e0e498
Ravie <ravijkee@yandex.ru>
2019-07-18 22:30:38 +0300

    Use ServletUriComponentsBuilder for saving query parameters when go to another page

--- src/main/java/com/github/department/controller/PageController.java
+++ src/main/java/com/github/department/controller/PageController.java
@@ -1,18 +1,20 @@
 package com.github.department.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 public class PageController {
     @PostMapping("/goToPage")
-    public String goToPage(@RequestParam Integer page,
-                           @RequestParam Integer size,
-                           @RequestParam String url,
-                           Model model) {
-        model.addAttribute("pageNumber", page);
-        return String.format("redirect:%s?page=%s&size=%s", url, page - 1, size);
+    public String goToPage(@RequestParam(required = false, defaultValue = "") Integer pageNumber,
+                           @RequestParam String uri) {
+        String newUri = ServletUriComponentsBuilder.fromUriString(uri).replaceQueryParam("page", ((pageNumber != null) ? pageNumber : 1) - 1).toUriString();
+        UriComponentsBuilder urlBuilder = UriComponentsBuilder.fromHttpUrl(newUri);
+        urlBuilder.scheme(null);
+
+        return "redirect:/" + urlBuilder.toUriString();
     }
 }
